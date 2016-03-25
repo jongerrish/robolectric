b45378e6c2ebce1baa4500222f6b70dd73d26eb0
Jonathan Gerrish <jongerrish@google.com>
2016-03-24 17:11:54 -0700

    Make ShadowInputMethodManager work by calling through by default and removing a shadowed method.

--- robolectric-shadows/shadows-core/src/main/java/org/robolectric/shadows/ShadowInputMethodManager.java
+++ robolectric-shadows/shadows-core/src/main/java/org/robolectric/shadows/ShadowInputMethodManager.java
@@ -4,23 +4,17 @@ import android.os.IBinder;
 import android.os.ResultReceiver;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+
 import org.robolectric.annotation.Implementation;
 import org.robolectric.annotation.Implements;
-import org.robolectric.annotation.HiddenApi;
-import org.robolectric.internal.Shadow;
 
 /**
  * Shadow for {@link android.view.inputmethod.InputMethodManager}.
  */
-@Implements(value = InputMethodManager.class, callThroughByDefault = false)
+@Implements(value = InputMethodManager.class)
 public class ShadowInputMethodManager {
 
   private boolean softInputVisible;
-
-  @HiddenApi @Implementation
-  static public InputMethodManager peekInstance() {
-    return Shadow.newInstanceOf(InputMethodManager.class);
-  }
 
   @Implementation
   public boolean showSoftInput(View view, int flags) {
