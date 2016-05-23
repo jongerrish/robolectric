f8ace9d8ff36f2becabdbfeaba9cc645a5dd09ce
Jonathan Gerrish <jongerrish@google.com>
2016-05-23 10:36:56 -0700

    Revert "Synchronize the plan cache in ShadowWrangler (#2458)"
    This reverts commit 85763ea2049f8297876d87fbc901be061a626ecb.
    
    This seemed to cause deadlocks in our builds.

--- robolectric/src/main/java/org/robolectric/internal/bytecode/ShadowWrangler.java
+++ robolectric/src/main/java/org/robolectric/internal/bytecode/ShadowWrangler.java
@@ -44,13 +44,12 @@ public class ShadowWrangler implements ClassHandler {
   private static final MethodHandle NO_SHADOW_HANDLE = constant(Object.class, NO_SHADOW);
   private final ShadowMap shadowMap;
   private final Map<Class, MetaShadow> metaShadowMap = new HashMap<>();
-  private final Map<String, Plan> planCache =
-      Collections.synchronizedMap(new LinkedHashMap<String, Plan>() {
-        @Override
-        protected boolean removeEldestEntry(Map.Entry<String, Plan> eldest) {
-          return size() > 500;
-        }
-      });
+  private final Map<String, Plan> planCache = new LinkedHashMap<String, Plan>() {
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<String, Plan> eldest) {
+      return size() > 500;
+    }
+  };
   private final Map<Class, ShadowConfig> shadowConfigCache = new ConcurrentHashMap<>();
   private final ClassValue<ShadowConfig> shadowConfigs = new ClassValue<ShadowConfig>() {
     @Override protected ShadowConfig computeValue(Class<?> type) {
@@ -127,11 +126,8 @@ public class ShadowWrangler implements ClassHandler {
 
   @Override
   public Plan methodInvoked(String signature, boolean isStatic, Class<?> theClass) {
-    Plan plan = planCache.get(signature);
-    if (plan != null) {
-      return plan;
-    }
-    plan = calculatePlan(signature, isStatic, theClass);
+    if (planCache.containsKey(signature)) return planCache.get(signature);
+    Plan plan = calculatePlan(signature, isStatic, theClass);
     planCache.put(signature, plan);
     return plan;
   }
